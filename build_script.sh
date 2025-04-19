add9d88080d167b42cdc534f168728c0cf157c28
Franco <franco@debian.casa>
2025-04-19 16:28:42 +0200

    Installo build_script.sh e modifico netlify.toml per pagefind

---
+++ build_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Comando per buildare il sito Hugo
+
+hugo --gc --minify -b $URL
+
+echo "Build di Hugo completata."
+
+# Esempio di post-processing: copia di file aggiuntivi
+echo "Eseguo post-processing con comando Python"
+python3 -m pagefind --site public
+
+echo "Post-processing completato."

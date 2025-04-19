fc4bfe29b13d8378995149d5fb8bebf231f33a20
Franco <franco@debian.casa>
2025-04-19 16:41:10 +0200

    Modifico build_script.sh con aggiunta di pip install pagefind

--- build_script.sh
+++ build_script.sh
@@ -8,6 +8,7 @@ echo "Build di Hugo completata."
 
 # Esempio di post-processing: copia di file aggiuntivi
 echo "Eseguo post-processing con comando Python"
+pip install pagefind
 python3 -m pagefind --site public
 
 echo "Post-processing completato."
